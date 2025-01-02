2be76c98b2f457fa281d3ddbd5bb7e1756a916ae
Dragonlord1005 <dragonlord100587@gmail.com>
2025-01-02 00:36:27 -0700

    Enable Bluetooth support and auto Numlock on startup in NixOS configuration

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -137,6 +137,12 @@
     # catppuccin-kvantum
   ];
 
+ # Enable Bluetooth support
+  hardware.bluetooth.enable = true;
+
+  # Enable numlock on startup
+  services.displayManager.sddm.autoNumlock = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
