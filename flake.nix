c5c3c36df3210c3f47005782706b14cb929bc581
Dragonlord1005 <dragonlord100587@gmail.com>
2025-01-02 01:56:09 -0700

    Remove unused Sweet Mars theme configuration from NixOS setup

--- flake.nix
+++ flake.nix
@@ -25,7 +25,6 @@
         # Import the previous configuration.nix we used,
         # so the old configuration file still takes effect
         ./nixos/configuration.nix
-        # ./nixos/sweet-mars-sddm.nix
 
         home-manager.nixosModules.home-manager
           {
