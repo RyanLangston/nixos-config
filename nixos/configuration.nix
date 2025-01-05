a713dd4f2ae6d4f11a653c643f72870ab7f83330
Dragonlord1005 <dragonlord100587@gmail.com>
2025-01-05 16:27:00 -0700

    Add static hostname mapping for local network devices

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -28,6 +28,10 @@
   networking.hostName = "bagelmachine"; # Define your hostname.
   #networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
+  networking.hosts = {
+    "192.168.1.104" = [ "tower.local" "tower" ];
+  };
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
