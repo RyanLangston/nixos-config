216f89f8de58e48e4e3e245b86b9efe4bce2e658
Dragonlord1005 <dragonlord100587@gmail.com>
2025-01-05 14:51:34 -0700

    Add MangoHud, GOverlay, and ProtonUp-Qt to home.nix package list

--- home.nix
+++ home.nix
@@ -25,6 +25,9 @@
     pkgs.tldr
     pkgs.vscode
     pkgs.meslo-lgs-nf
+    pkgs.mangohud
+    pkgs.goverlay
+    pkgs.protonup-qt
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
