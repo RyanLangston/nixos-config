a19eadc32ea233fcc7ba49b37d994cbb1daaa206
Dragonlord1005 <dragonlord100587@gmail.com>
2025-01-05 15:02:35 -0700

    Add systemd user service reload configuration to home.nix

--- home/home.nix
+++ home/home.nix
@@ -101,7 +101,10 @@
   # Font Configuration
   fonts = { 
     fontconfig.enable = true;    
-  };  
+  };
+
+  # Nicely reload system units when changing configs
+  systemd.user.startServices = "sd-switch";
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
