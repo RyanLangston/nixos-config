fe1e0da9d3daceb42e8e28304162f424fb5fb8e0
Dragonlord1005 <dragonlord100587@gmail.com>
2025-01-05 21:48:17 -0700

    Add youtube-music package to home-manager configuration

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -28,6 +28,7 @@
     pkgs.mangohud
     pkgs.goverlay
     pkgs.protonup-qt
+    pkgs.youtube-music
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
