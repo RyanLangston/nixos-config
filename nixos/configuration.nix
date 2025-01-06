c41016d7f578e5d2d02b9c7ec207ff5685bb4f92
Dragonlord1005 <dragonlord100587@gmail.com>
2025-01-06 00:47:39 -0700

    Enable Waydroid virtualization in NixOS configuration

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -101,7 +101,7 @@
 
   services.tailscale.enable = true;
 
-  virtualisation.waydroid.enable
+  virtualisation.waydroid.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
