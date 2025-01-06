12f23defdc151ca6ba5dff8933fa8c1059a3fb89
Dragonlord1005 <dragonlord100587@gmail.com>
2025-01-06 11:19:13 -0700

    Enable Podman virtualization

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -101,6 +101,8 @@
 
   services.tailscale.enable = true;
 
+  virtualization.podman.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
