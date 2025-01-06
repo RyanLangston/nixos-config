66b349a74f740a5366de8f0a99b2e648beb2bcff
Dragonlord1005 <dragonlord100587@gmail.com>
2025-01-06 13:15:46 -0700

    Remove unnecessary autocommands for Nix file type indentation

--- home-manager/nixvim/autocommands.nix
+++ home-manager/nixvim/autocommands.nix
@@ -1,10 +1,3 @@
 {
-  programs.nixvim.autoCmd = [
-	{
-		group = "FileTypeIndent";
-		pattern = "nix";
-		# We need to set indent size to two for nixvim
-		command = "setlocal shiftwidth=2";
-	}
-  ];
+
 }
