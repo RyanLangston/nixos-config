79a7a43ad0ac7c08ef8851334f5bbe4b260f8786
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2025-01-10 15:34:06 -0700

    Added nix-index

--- flake.nix
+++ flake.nix
@@ -21,6 +21,8 @@
 
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nix-index-database.url = "github:nix-community/nix-index-database";
+    nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = {
@@ -81,6 +83,7 @@
         modules = [
           # > Our main home-manager configuration file <
           ./home-manager/home.nix
+          inputs.nix-index-database.hmModules.nix-index
         ];
       };
     };
--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,10 +1,7 @@
 # This is your home-manager configuration file
 # Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
 {
-  inputs,
   outputs,
-  lib,
-  config,
   pkgs,
   ...
 }: {
@@ -85,6 +82,7 @@
         theme = "robbyrussell";
       };
     };
+    nix-index.enable = true;
   };
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
