9f577649cff13d351d563a0b3f1979afb40b6ff8
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2025-01-12 11:40:21 -0700

    removed todos

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -96,7 +96,6 @@
       openssh.authorizedKeys.keys = [
         # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
       ];
-      # TODO: Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
       extraGroups = ["wheel" "networkmanager" "audio" "libvirtd"];
       shell = pkgs.zsh;
     };
